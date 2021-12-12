913a3b17c3a6ea690792dcf1ede38552522aa0dd
Khoa Nguyen <nguyentrananhkhoa012@gmail.com>
2021-12-12 17:09:38 +0700

    initialize

---
+++ Persistence/Migrations/20211211134358_remove-test-add-property.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class removetestaddproperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestProperty",
+                table: "Activities");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TestProperty",
+                table: "Activities",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
